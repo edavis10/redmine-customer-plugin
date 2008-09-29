26453e09e74080e17be9c10f29101dd9b4033c7c
Michele Franzin <michele.franzin@gmail.com>
2008-09-29 11:36:31 +0200

    made name or company mandatory

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,23 @@
 class Customer < ActiveRecord::Base
   has_many :projects
   
+  # name or company is mandatory
+  validates_presence_of :name, :if => :company_unsetted
+  validates_presence_of :company, :if => :name_unsetted
+  
   def pretty_name
     result = name || "[#{company}]" 
     result << " [#{company}]" unless company.blank?
   end
+  
+  private
+  
+  def name_unsetted
+    self.name.blank?
+  end
+  
+  def company_unsetted
+    self.company.blank?
+  end
+
 end
