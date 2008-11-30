d145f7fdee7d29645e2f911987f6f73eb8dabc64
Eric Davis <edavis@littlestreamsoftware.com>
2008-11-30 15:28:48 -0800

    Added RSpec specs to test the Customer validations.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,2 +1,123 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+describe Customer do
+  before(:each) do
+    @customer = Customer.new
+  end
+  
+  it 'should not be valid without a name or company' do
+    @customer.should_not be_valid
+  end
+  
+  it 'should require a name if company is not set' do
+    @customer.should_not be_valid
+    @customer.errors.on(:name).should include("activerecord_error_blank")
+  end
+
+  it 'should require a company if name is not set' do
+    @customer.should_not be_valid
+    @customer.errors.on(:company).should include("activerecord_error_blank")
+  end
+  
+  it 'should be valid if name is set but company is missing' do
+    @customer.name = "John Customer"
+    @customer.should be_valid
+  end
+
+  it 'should be valid if company is set but name is missing' do
+    @customer.company = "ABC Company"
+    @customer.should be_valid
+  end
+
+  it '.name should be unique within a company' do
+    @customer.name = 'John Customer'
+    @customer.company = "ABC Company"
+    @customer.save!
+    
+    @conflict_customer = Customer.new( :name => 'John Customer', :company => "ABC Company")
+    @conflict_customer.should_not be_valid
+    @conflict_customer.errors.on(:name).should include("activerecord_error_taken")
+  end
+  
+  describe 'with a valid email' do
+    it 'should have a single @' do
+      @customer.attributes = { :name => 'John Customer', :company => "ABC Company"}
+      @customer.email = ""
+      @customer.should be_valid
+    end
+      
+
+    it 'should have at least one character before @'
+    it 'should have alphanumerics or hypens after for the domain'
+    it 'should allow subdomains'
+    it 'should have a TLD at least 2 characters long'
+  end
+  
+  describe 'with an invalid email' do
+    before(:each) do
+      @customer.attributes = { :name => 'John Customer', :company => "ABC Company"}
+    end
+    
+    it 'would have more than one @' do
+      @customer.email = "john@ABC@com"
+      @customer.should_not be_valid
+    end
+
+    it 'would have no characters before @' do
+      @customer.email = "@com"
+      @customer.should_not be_valid
+    end
+
+    it 'would have no characters after @' do
+      @customer.email = "john@"
+      @customer.should_not be_valid
+    end
+
+    it 'would have non-alphanumerics for the domain' do
+      @customer.email = "john@ABC,DEF.com"
+      @customer.should_not be_valid
+    end
+
+    it 'should have a TLD at least 2 characters long' do
+      @customer.email = "john@ABC.c"
+      @customer.should_not be_valid
+    end
+
+  end
+  
+  describe 'with a valid email' do
+    it 'can be blank' do
+      @customer.attributes = { :name => 'John Customer', :company => "ABC Company"}
+      @customer.email = ""
+      @customer.should be_valid
+    end
+    
+    it 'can be nil' do
+      @customer.attributes = { :name => 'John Customer', :company => "ABC Company"}
+      @customer.email = nil
+      @customer.should be_valid
+    end
+    
+    it 'should allow subdomains' do
+      @customer.attributes = { :name => 'John Customer', :company => "ABC Company"}
+      @customer.email = "john@sales.abc.com"
+      @customer.should be_valid
+    end
+    
+    it 'should allow a TLD with at least 2 characters' do
+      @customer.attributes = { :name => 'John Customer', :company => "ABC Company"}
+      @customer.email = "john@abc.us"
+      @customer.should be_valid
+    end
+  end
+  
+end
+
+describe Customer do
+  it 'should have many projects'
+end
+
+describe Customer, '.pretty_name' do
+  it 'should return the name of the Customer'
+  it 'should add the company to the name if company is present'
+end
